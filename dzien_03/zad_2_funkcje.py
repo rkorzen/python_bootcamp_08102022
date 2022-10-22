9f529e7d7b494624a0d2b9bea238e077d5009074
rkorzen <korzeniewski@gmail.com>
2022-10-22 16:50:53 +0200

    wiecej niz

---
+++ dzien_03/zad_2_funkcje.py
@@ -0,0 +1,32 @@
+"""
+Napisz funkcję zwracającą zbiór wszystkich znaków występujących w
+napisie więcej niż zadana liczba razy.
+
+Przykład użycia:
+
+>>> wiecej_niz('ala ma kota a kot ma ale', 3)
+{"a", ' '}
+
+>>> wiecej_niz('', 3)
+set()
+
+
+assert wiecej_niz('', 3 ) == set()
+
+>>> wiecej_niz('aaa', 3)
+set()
+
+>>> wiecej_niz('aaa', 0)
+{"a"}
+
+>>> wiecej_niz('bbb', 2)
+{"b"}
+
+>>> wiecej_niz('aaaa', 3)
+{'a'}
+
+
+"""
+
+def wiecej_niz(a, b):
+    return set()
--- dzien_03/zad_l_pierwsze.py
+++ dzien_03/zad_l_pierwsze.py
@@ -3,12 +3,15 @@ Zdefiniuj funkcję która sprawdzi czy dana liczba jest liczbą pierwszą
 
 """
 
-def czy_pierwsza(arg):
-    return False
+def czy_pierwsza(arg: int) -> bool:
+    for dzielnik in range(2, arg):
+        if arg % dzielnik == 0:
+            return False
+    return True
 
 
 assert czy_pierwsza(10) is False
 assert czy_pierwsza(2) is True
-# assert czy_pierwsza(3) is True
-# assert czy_pierwsza(4) is False
+assert czy_pierwsza(3) is True
+assert czy_pierwsza(4) is False
 assert 3 % 2 == 1
