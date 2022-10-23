375bf597f9fec2b3cc850e8afc8a486cc6894db5
rkorzen <korzeniewski@gmail.com>
2022-10-23 11:31:57 +0200

    rozwiązanie początkującego

--- dzien_04/kalkulator.py
+++ dzien_04/kalkulator.py
@@ -10,3 +10,21 @@ $ python kalkulator.py
 Wynik to: 5
 
 """
+
+def kalkulator() -> int:
+    # print("Jaką operację chcesz wykona? [1-dodaj, 2-odejmowanie, 3-mnożenie, 4-dzielenie]:")
+    operacja = input("Jaką operację chcesz wykona? [1-dodaj, 2-odejmowanie, 3-mnożenie, 4-dzielenie]:")
+    a = int(input("Podaj liczbę 1"))
+    b = int(input("Podaj liczbę 2"))
+
+    if operacja == "1":
+        print(a + b)
+    elif operacja == "2":
+        print(a - b)
+    elif operacja == "3":
+        print(a * b)
+    elif operacja == "4":
+        print(a / b)
+
+
+kalkulator()
