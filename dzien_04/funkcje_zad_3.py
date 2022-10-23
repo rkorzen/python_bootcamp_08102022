58506e9d6f979d523b3ba0ee87f0d4394f70e2b6
rkorzen <korzeniewski@gmail.com>
2022-10-23 10:21:34 +0200

    Zadanie 3 - przed refactoringiem

--- dzien_04/funkcje_zad_3.py
+++ dzien_04/funkcje_zad_3.py
@@ -9,14 +9,40 @@ Przykład użycia:
 
 >>> policz_znaki('ala ma <kota> a kot ma ale')
 4
+
 >>> policz_znaki('ala ma <kota> a <kot> ma ale')
 7
+
+>>> policz_znaki('ala ma [kota] a [kot] ma ale', "[", "]")
+7
+
 >>> policz_znaki('ala [kota [a kot]] ma [ale]', '[', ']')
 18
 
 >>> policz_znaki('ala [kota [a kot]] ma [ale]', start='[', end=']')
 18
+
 >>> policz_znaki('a <a<a<a>>>')
 6
 
 """
+
+
+def policz_znaki(napis: str, start: str = "<", end: str = ">") -> int:
+    licznik = 0
+    czy_zliczac = False
+    poziom = 0
+    # 'ala ma <kota> a kot ma ale'
+    for znak in napis:
+        if znak == start:
+            czy_zliczac = True
+            poziom += 1
+            continue
+        elif znak == end:
+            czy_zliczac = False
+            poziom -= 1
+
+        if czy_zliczac:
+            licznik += poziom
+
+    return licznik
