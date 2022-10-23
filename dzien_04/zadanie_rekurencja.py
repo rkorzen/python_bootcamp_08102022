92cbcbe1f4a377f74d0b881b661f6237559fd178
rkorzen <korzeniewski@gmail.com>
2022-10-23 14:03:08 +0200

    splascz

---
+++ dzien_04/zadanie_rekurencja.py
@@ -0,0 +1,15 @@
+"""
+
+Zaimplementuj funkcję spłaszczającą podaną listę.
+Przykład użycia:
+
+>>> splaszcz([1, 2, 3, [4, 5, [6]], 7])
+[1, 2, 3, 4, 5, 6, 7]
+
+>>> splaszcz([1, 2, 3])
+[1, 2, 3]
+
+>>> splaszcz([1, [[2, 3]]])
+[1, 2, 3]
+
+"""
