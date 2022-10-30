3ba8b73eecd5f8014f2d2d9c1e83734b03a45db6
rkorzen <korzeniewski@gmail.com>
2022-10-30 09:33:54 +0100

    clean emails

--- dzien_05/zadanie_czysc_emaile.py
+++ dzien_05/zadanie_czysc_emaile.py
@@ -19,4 +19,32 @@ with open(sciezka, mode="tryb") as f:
     f.write()
 """
 
-print("  Ala ma kota  ".count("@"))
+def get_data(filename: str) -> list[str]:
+    with open(filename) as f:
+        data = f.readlines()
+        return data
+
+def clean_data(data: list[str]) -> list[str]:
+    return sorted({email.strip().lower() for email in data if email.count("@") == 1})
+
+
+def write_data_line_by_line(filename: str, data: list[str]):
+    with open(filename, "w") as f:
+        for el in data:
+            f.write(el+"\n")
+
+def write_data_once(filename: str, data: list[str]):
+    with open(filename, "w") as f:
+        data = "\n".join(data)
+        f.write(data)
+
+def main():
+    data = get_data("data/emails.txt")
+    data = clean_data(data)
+    write_data_once("data/cleaned_emails.txt", data)
+
+
+
+if __name__ == "__main__":
+    main()
+
